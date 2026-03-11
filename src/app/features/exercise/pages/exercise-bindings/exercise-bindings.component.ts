c32f6f994b61d5d062bede75bcc1378735da6e88
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-11 16:50:06 +0100

    Correction bindings

--- src/app/features/exercise/pages/exercise-bindings/exercise-bindings.component.ts
+++ src/app/features/exercise/pages/exercise-bindings/exercise-bindings.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
+import {FormsModule} from "@angular/forms";
 import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
 
 @Component({
 	selector: "exercise-bindings",
-	imports: [ExerciseHeaderComponent],
+	imports: [ExerciseHeaderComponent, FormsModule],
 	templateUrl: "./exercise-bindings.component.html",
 	styleUrl: "./exercise-bindings.component.scss",
 })
@@ -11,4 +12,20 @@ export class ExerciseBindingsComponent {
 	imageUrlResultat: string = "assets/exercise/binding/resultat.png";
 
 	// Rajoute les variables et méthodes nécessaires ici
+	prenom = "";
+	nom = "";
+	nomComplet = "";
+	isSubmitDisabled = true;
+
+	onKeyUp() {
+		this.nomComplet = `${this.prenom} ${this.nom}`;
+
+		if (!this.prenom || !this.nom) {
+			this.isSubmitDisabled = true;
+		} else {
+			this.isSubmitDisabled = false;
+		}
+
+		// this.isSubmitDisabled = !this.prenom || !this.nom
+	}
 }
