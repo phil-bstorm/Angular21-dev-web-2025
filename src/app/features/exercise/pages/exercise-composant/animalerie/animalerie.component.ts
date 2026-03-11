4a1ac60260f39ecf0e911f8ff7417737ab64d40d
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-11 09:58:55 +0100

    Correction exercice composant

--- src/app/features/exercise/pages/exercise-composant/animalerie/animalerie.component.ts
+++ src/app/features/exercise/pages/exercise-composant/animalerie/animalerie.component.ts
@@ -1,8 +1,10 @@
 import {Component} from "@angular/core";
+import {ChatComponent} from "./components/chat/chat.component";
+import {ChienComponent} from "./components/chien/chien.component";
 
 @Component({
 	selector: "animalerie",
-	imports: [],
+	imports: [ChatComponent, ChienComponent],
 	templateUrl: "./animalerie.component.html",
 	styleUrl: "./animalerie.component.scss",
 })
