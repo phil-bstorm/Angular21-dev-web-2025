35955696a0625c2e7199b5de1244b2a9d69bb226
Phil BStorm <philippe.haerens@bstorm.be>
2026-03-13 10:37:16 +0100

    correction directive

--- src/app/features/exercise/pages/exercise-directive/exercise-directive.component.ts
+++ src/app/features/exercise/pages/exercise-directive/exercise-directive.component.ts
@@ -1,10 +1,41 @@
 import {Component} from "@angular/core";
 import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
+import {Product} from "./models/product.interface";
+import {NgClass} from "@angular/common";
 
 @Component({
 	selector: "exercise-directive",
-	imports: [ExerciseHeaderComponent],
+	imports: [ExerciseHeaderComponent, NgClass],
 	templateUrl: "./exercise-directive.component.html",
 	styleUrl: "./exercise-directive.component.scss",
 })
-export class ExerciseDirectiveComponent {}
+export class ExerciseDirectiveComponent {
+	produits: Array<Product> = [
+		{
+			nom: "iPhone 15 Pro",
+			photo: "https://images.unsplash.com/photo-1696446701796-da61225697cc?w=400",
+			prix: 1199.99,
+			qttRestante: 12,
+		},
+		{
+			nom: "Samsung Galaxy S24",
+			photo: "https://images.unsplash.com/photo-1610945415295-d9bbf067e59c?w=400",
+			prix: 899.99,
+			qttRestante: 25,
+		},
+		{
+			nom: "MacBook Air M3",
+			photo: "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?w=400",
+			prix: 1299.99,
+			prixPromo: 1149.99,
+			qttRestante: 0,
+		},
+		{
+			nom: "Sony WH-1000XM5",
+			photo: "https://images.unsplash.com/photo-1618366712010-f4ae9c647dcb?w=400",
+			prix: 379.99,
+			prixPromo: 279.99,
+			qttRestante: 34,
+		},
+	];
+}
---
+++ src/app/features/exercise/pages/exercise-directive/models/product.interface.ts
@@ -0,0 +1,7 @@
+export interface Product {
+	nom: string;
+	photo: string;
+	prix: number;
+	prixPromo?: number;
+	qttRestante: number;
+}
